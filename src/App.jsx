8751c8bb15a85c2cfb4c7b6e37a11b6f9dee2811
AbubakkarSiddhik <abubakkarsiddhiq54321@gmail.com>
2025-03-20 16:48:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 // import { Button, TextField, Card, CardContent, Typography } from "@mui/material";
 // import { addDoc, collection, deleteDoc, doc, getDocs, query, updateDoc } from "firebase/firestore";
 // import MyContext from './MyContext';
