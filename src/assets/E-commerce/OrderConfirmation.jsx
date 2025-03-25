b5543b9cc42bbfef3072421a9d61b30056b392b6
AbubakkarSiddhik <abubakkarsiddhiq54321@gmail.com>
2025-03-25 17:46:47 +0530

    Update OrderConfirmation.jsx

--- src/assets/E-commerce/OrderConfirmation.jsx
+++ src/assets/E-commerce/OrderConfirmation.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const OrderConfirmation = () => {
   return (
-    <div className="max-w-lg mx-auto p-6 mt-20 bg-white shadow-lg rounded-lg text-center">
+    <div className="max-w-lg mx-auto p-6 mt-20 bg-white shadow-lg rounded-lg text-center bg-gradient-to-r from-blue-50 to-purple-50">
       <h2 className="text-3xl font-bold text-green-600">🎉 Thank You for Your Order!</h2>
       <p className="mt-2 text-gray-600">Your order has been placed successfully.</p>
       <p className="mt-1 text-gray-500">We will send you a confirmation email shortly.</p>
