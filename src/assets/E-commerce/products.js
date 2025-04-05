4d7b13bd2fce02b0397eb07a045e0229daee8d8a
AbubakkarSiddhik <abubakkarsiddhiq54321@gmail.com>
2025-04-05 14:02:52 +0530

    Update products.js

--- src/assets/E-commerce/products.js
+++ src/assets/E-commerce/products.js
@@ -9,12 +9,13 @@ import sunGlass from "./sunGlass.jpg";
 import formalShoe from "./formalShoe.jpg";
 
 export const products = [
-    { id: 1, name: "Stylish Jacket", price: 79.99, category: "Men", image: jacket },
-    { id: 2, name: "Elegant Dress", price: 99.99, category: "Women", image: elegantDress },
-    { id: 3, name: "Running Shoes", price: 59.99, category: "Shoes", image: runningShoe },
-    { id: 4, name: "Luxury Watch", price: 199.99, category: "Accessories", image: watch },
-    { id: 5, name: "Casual T-Shirt", price: 29.99, category: "Men", image: tshirt },
-    { id: 6, name: "Handbag", price: 89.99, category: "Women", image: handBag },
-    { id: 7, name: "Sunglasses", price: 49.99, category: "Accessories", image: sunGlass },
-    { id: 8, name: "Formal Shoes", price: 119.99, category: "Shoes", image: formalShoe },
+    { id: 1, name: "Stylish Jacket", price: 6640, category: "Men", image: jacket },
+    { id: 2, name: "Elegant Dress", price: 8310, category: "Women", image: elegantDress },
+    { id: 3, name: "Running Shoes", price: 4970, category: "Shoes", image: runningShoe },
+    { id: 4, name: "Luxury Watch", price: 16599, category: "Accessories", image: watch },
+    { id: 5, name: "Casual T-Shirt", price: 2480, category: "Men", image: tshirt },
+    { id: 6, name: "Handbag", price: 7420, category: "Women", image: handBag },
+    { id: 7, name: "Sunglasses", price: 4110, category: "Accessories", image: sunGlass },
+    { id: 8, name: "Formal Shoes", price: 9950, category: "Shoes", image: formalShoe },
 ];
+
