1e019948b9c7bcbe917b4bc159e6e1eec95ca146
AbubakkarSiddhik <abubakkarsiddhiq54321@gmail.com>
2025-04-19 17:32:40 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,20 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore"; // Added for Firestore
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
-// Replace with your actual Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyCMfnb5_2h98MA3FYFyWe2BD3wSKd1pmD0",
   authDomain: "megamart-b0545.firebaseapp.com",
   projectId: "megamart-b0545",
-  storageBucket: "megamart-b0545.firebasestorage.app",
+  storageBucket: "megamart-b0545.appspot.com",
   messagingSenderId: "226342898198",
   appId: "1:226342898198:web:601a25cbfbb1c24a004337",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Auth and Google Provider
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
-export const db = getFirestore(app); // Added Firestore export
+export const db = getFirestore(app);
+export const storage = getStorage(app);
