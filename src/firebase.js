c1b140e211981bd558dff88652d4dfb494974a29
AbubakkarSiddhik <abubakkarsiddhiq54321@gmail.com>
2025-04-21 17:59:41 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCMfnb5_2h98MA3FYFyWe2BD3wSKd1pmD0",
   authDomain: "megamart-b0545.firebaseapp.com",
   projectId: "megamart-b0545",
-  storageBucket: "megamart-b0545.appspot.com",
+  storageBucket: "megamart-b0545.firebasestorage.app",
   messagingSenderId: "226342898198",
   appId: "1:226342898198:web:601a25cbfbb1c24a004337",
+  measurementId: "G-YNCP2M3V8T"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -17,4 +17,3 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 export const db = getFirestore(app);
-export const storage = getStorage(app);
